b7aeed54ee68900043fc876669f2e218695876df
Jette Hernandez <jette.hernan@gmail.com>
2016-11-30 21:50:17 +0000

    created details files, played with css. Need to fix style

---
+++ constants.js
@@ -0,0 +1,1 @@
+var api_key = 'keyb5yyG63cFxlXVF';
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var api_key = 'keyb5yyG63cFxlXVF';
-var exercise_records = 'https://api.airtable.com/v0/appLn9msSiaJG7FNR/Exercises?api_key=' + api_key;
+var exercise_records = 'https://api.airtable.com/v0/appLn9msSiaJG7FNR/Exercises?api_key=' + api_key +'&sort%5B0%5D%5Bfield%5D=Body+Group&sort%5B0%5D%5Bdirection%5D=asc';
 
 function renderRecords(data) {
     //console.log(data)
@@ -11,13 +11,15 @@ function renderRecords(data) {
       var exercise_pics = exercise.fields['Attachments']
       var exercise_info = ''
       if (exercise_name) {
-        exercise_info +=`<li>`
+        exercise_info +=`<li class='row'>`
         if (exercise) {
           $.each(exercise_pics, function(i, pic){
-            exercise_info +=`<img src="${pic.url}">`;
+            exercise_info +=`<img src="${pic.url}"><br>`;
           });
         }
-        exercise_info += ` Exercise: ${exercise_name} <br> Body Group: ${exercise_bodygroup} <br> Movement: ${exercise_movement}`
+        exercise_info += ` Exercise: ${exercise_name}`
+        exercise_info += ` <br> Body Group: ${exercise_bodygroup}`
+        exercise_info += ` <br> Movement: ${exercise_movement}`
         exercise_info +=`</li>`;
       }
       $('.exercises').append(exercise_info);
